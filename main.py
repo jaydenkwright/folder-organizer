8a9187f91b19c06169b227b8c7f3e2f669291a31
jaydenkwright <jaydenkwright3@gmail.com>
2020-04-28 13:10:52 -0400

    Files are organized into folders to based on file category and year

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 import os
 import shutil
+import datetime
 from pathlib import Path
 import filetype
 
 path = os.getcwd()
+date = datetime.datetime.now()
 
 for r, d, f in os.walk(path + "/downloads/"):
     for file in f:
         if os.path.exists(path + "/downloads/" + file):
-            'print(file)'
             kind = filetype.guess(path + "/downloads/" + file)
-            'print(Path(file).suffix)'
             if kind is not(None):
                 new_folder = str(kind.mime).split("/")
                 if os.path.exists(path + "/downloads/" + str(new_folder[0])):
-                    print("exists")
-                    if file:
-                        shutil.move(path + "/downloads/" + file, path + "/downloads/" + new_folder[0] + "/" + file)
-                        print("Moved to /downloads/" + file, path + "/downloads/" + new_folder[0] + "/" + file)
+                    print("Path exists")
                 else:
                     os.makedirs(path + "/downloads/" + str(new_folder[0]))
-                    print('does not exist')
+                    print('Path created')
+                if os.path.exists(f"{path}/downloads/{str(new_folder[0])}/{date.year}/"):
+                    print("Path exists")
+                else:
+                    os.makedirs(f"{path}/downloads/{new_folder[0]}/{date.year}/")
+                    print("Path Created")
+
+                #shutil.move(path + "/downloads/" + file, path + "/downloads/" + new_folder[0] + "/" + file)
+                shutil.move(f"{path}/downloads/{file}", f"{path}/downloads/{new_folder[0]}/{date.year}/{file}")
+                print(f"Moved to /downloads/{file} to {path} + /downloads/ {new_folder[0]} / + {file}")
             else:
                 print('Cannot guess file type!' + file)
-                if not os.path.exists(path + "/downloads/others"):
-                    os.makedirs(path + "/downloads/others")
-                    print('does not exist')
-                else:
-                    print('exists')
-    else:
-        print('file does not exist')
+                # if not os.path.exists(path + "/downloads/others"):
+                #     os.makedirs(path + "/downloads/others")
+                #     print('does not exist')
+                # else:
+                #     print('exists')
+        else:
+            print('Not in root directory' + file)
