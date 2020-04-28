34a9a6a23dc0c6dd87490152434b857ba80459a4
jaydenkwright <jaydenkwright3@gmail.com>
2020-04-27 22:20:40 -0400

    Checks if folder for file type exists, if not it creates it. If the file type is not found it creates others folder

--- main.py
+++ main.py
@@ -8,7 +8,19 @@ for r, d, f in os.walk(path + "/downloads/"):
     for file in f:
         kind = filetype.guess(path + "/downloads/" + file)
         'print(Path(file).suffix)'
-        if kind is None:
-            print('Cannot guess file type!')
+        if kind is not(None):
+            split_kind = str(kind.mime).split("/")
+            new_folder = split_kind[0]
+            if not os.path.exists(path + "/downloads/" + str(new_folder)):
+                os.makedirs(path + "/downloads/" + str(new_folder))
+                print('does not exist')
+            else:
+                print('exists')
 
-        print(str(kind.mime))
+        else:
+            print('Cannot guess file type!' + file)
+            if not os.path.exists(path + "/downloads/others"):
+                os.makedirs(path + "/downloads/others")
+                print('does not exist')
+            else:
+                print('exists')
