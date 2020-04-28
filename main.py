1571dfadc91c9ab12347043773894102225e2c36
jaydenkwright <jaydenkwright3@gmail.com>
2020-04-27 23:14:44 -0400

    Files are moved to folder based on filetype

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from pathlib import Path
 import filetype
 
@@ -6,23 +7,26 @@ path = os.getcwd()
 
 for r, d, f in os.walk(path + "/downloads/"):
     for file in f:
-        kind = filetype.guess(path + "/downloads/" + file)
-        'print(Path(file).suffix)'
-        if kind is not(None):
-            split_kind = str(kind.mime).split("/")
-            new_folder = split_kind[0]
-            if not os.path.exists(path + "/downloads/" + str(new_folder)):
-                os.makedirs(path + "/downloads/" + str(new_folder))
-                print('does not exist')
+        if os.path.exists(path + "/downloads/" + file):
+            'print(file)'
+            kind = filetype.guess(path + "/downloads/" + file)
+            'print(Path(file).suffix)'
+            if kind is not(None):
+                new_folder = str(kind.mime).split("/")
+                if os.path.exists(path + "/downloads/" + str(new_folder[0])):
+                    print("exists")
+                    if file:
+                        shutil.move(path + "/downloads/" + file, path + "/downloads/" + new_folder[0] + "/" + file)
+                        print("Moved to /downloads/" + file, path + "/downloads/" + new_folder[0] + "/" + file)
+                else:
+                    os.makedirs(path + "/downloads/" + str(new_folder[0]))
+                    print('does not exist')
             else:
-                print('exists')
-            
-            
-
-        else:
-            print('Cannot guess file type!' + file)
-            if not os.path.exists(path + "/downloads/others"):
-                os.makedirs(path + "/downloads/others")
-                print('does not exist')
-            else:
-                print('exists')
+                print('Cannot guess file type!' + file)
+                if not os.path.exists(path + "/downloads/others"):
+                    os.makedirs(path + "/downloads/others")
+                    print('does not exist')
+                else:
+                    print('exists')
+    else:
+        print('file does not exist')
