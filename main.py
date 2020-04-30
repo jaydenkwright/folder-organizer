2082a51f2b378ca0399f8f48e0e9f8010b48c1d4
jaydenkwright <jaydenkwright3@gmail.com>
2020-04-29 20:27:35 -0400

    Created move_files() function

--- main.py
+++ main.py
@@ -8,21 +8,31 @@ import filetype
 path = os.getcwd()
 date = datetime.datetime.now()
 
+# def create_folder(folder):
+    #os.makedirs(f"{path}/downloads/{new_folder[0]}/")
 
-def create_folder(folder):
-    if os.path.exists(folder):
-        print("path exists")
-    else:
-        os.makedirs(folder)
-        print("Path created")
+def new_folder_name(kind):
+    return str(kind.mime).split("/")
+    
 
 def create_folders(kind):
-    new_folder = str(kind.mime).split("/")
-    if os.path.exists(f"{path}/downloads/{new_folder[0]}/")
+    new_folder = new_folder_name(kind)
+    print(kind)
+    if os.path.exists(f"{path}/downloads/{new_folder[0]}/"):
         print("Path exists")
     else:
         os.makedirs(f"{path}/downloads/{new_folder[0]}/")
         print(f"{path}/downloads/{new_folder[0]}/ has been created")
+    if os.path.exists(f"{path}/downloads/{str(new_folder[0])}/{date.year}/"):
+        print("Path exists")
+    else:
+        os.makedirs(f"{path}/downloads/{str(new_folder[0])}/{date.year}/")
+        print(f"{path}/downloads/{str(new_folder[0])}/{date.year}/ has been created")
+
+def move_files(path, folder, file):
+    shutil.move(f"{path}/downloads/{file}", f"{path}/downloads/{folder[0]}/{date.year}/{file}")
+    print(f"Moved to /downloads/{file} to {path}/downloads/{folder[0]}/{file}")
+    
         
 
 def main():
@@ -33,10 +43,10 @@ def main():
                 kind = filetype.guess(path + "/downloads/" + file)
                 if kind is not(None):
                     new_folder = str(kind.mime).split("/")
-                    create_folder(f"{path}/downloads/{str(new_folder[0])}/")
-                    create_folder(f"{path}/downloads/{str(new_folder[0])}/{date.year}/")
-                    shutil.move(f"{path}/downloads/{file}", f"{path}/downloads/{new_folder[0]}/{date.year}/{file}")
-                    print(f"Moved to /downloads/{file} to {path}/downloads/{new_folder[0]}/{file}")
+                    create_folders(kind)
+                    # create_folder(f"{path}/downloads/{str(new_folder[0])}/")
+                    # create_folder(f"{path}/downloads/{str(new_folder[0])}/{date.year}/")
+                    move_files(path, new_folder, file)
                 else:
                     print('Cannot guess file type!' + file)
                     if not os.path.exists(f"{path}/downloads/others"):
